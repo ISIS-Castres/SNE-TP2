a3a21d929dfcc7b9bc910f8c2b40b522ff438fa3
ychasser <41200686+ychasser@users.noreply.github.com>
2025-08-29 22:59:31 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,8 +1,60 @@
 class Main {
   public static void main(String[] args) {
+    System.out.println("de l'ADN !!!");
 
-    System.out.println("Quaternion !");
-    
-    
+    System.out.println(estACGT('A'));
+    System.out.println(estACGT('Z'));
   }
+
+  /*
+   * fonction : estACGT
+   * données : un caractère -> paramètre -> char
+   * résultat : vrai ou faux -> valeur de retour -> booléen
+   */
+  public static boolean estACGT(char c) {
+    return c == 'A' || c == 'C' || c == 'G' || c == 'T';
+  }
+
+  /*
+   * fonction : estADN
+   * données :
+   * résultat :
+   */
+
+  /*
+   * fonction : masseMolaireBase
+   * données :
+   * résultat :
+   */
+
+  /*
+   * fonction : masseMolaire
+   * données :
+   * résultat :
+   */
+
+  /*
+   * fonction : complementaireBase
+   * données :
+   * résultat :
+   */
+
+  /*
+   * fonction : brinComp
+   * données :
+   * résultat :
+   */
+
+  /*
+   * fonction : nucleideHasard
+   * données :
+   * résultat :
+   */
+
+  /*
+   * fonction : genererSequenceADN
+   * données :
+   * résultat :
+   */
+
 }
